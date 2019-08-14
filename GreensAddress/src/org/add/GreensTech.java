83d462ecd915aa7d6a77c939196579cf03cc7041
User <User@DESKTOP-H3FN1VC>
2019-08-14 17:53:49 +0530

    method overloading

---
+++ GreensAddress/src/org/add/GreensTech.java
@@ -0,0 +1,26 @@
+package org.add;
+
+public class GreensTech {
+	public void greensOmr(String name,String street,String city) {
+		System.out.println(name+" "+street+" "+city);
+	}
+	public void greensOmr(String name,int floorNum) {
+		System.out.println(name+" "+floorNum);
+	}
+	public void greensOmr(String name,int floor,String street) {
+		System.out.println(name+" "+floor+" "+street);
+	}
+	public void greensOmr(String name,int floor,double income) {
+		System.out.println(name+" "+income+" "+floor);
+	}
+	
+	public static void main(String[] args) {
+		GreensTech obj=new GreensTech();
+		obj.greensOmr("Greens","OMR","Chennai");
+		obj.greensOmr("Greens",2,"OMR");
+		obj.greensOmr("Greens",2);
+		obj.greensOmr("Greens",2,500000.9);
+		
+	}
+
+}
